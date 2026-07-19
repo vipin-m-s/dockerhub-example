5f0fe9eedbb296c1ef8606ae96fddb7c54ca9086
Vipin M S <40431065+vipin-m-s@users.noreply.github.com>
2026-07-19 07:10:16 +0530

    Update login.sh

--- jenkins/login.sh
+++ jenkins/login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+echo $DOCKERHUB_PAT_PSW | docker login -u $DOCKERHUB_PAT_USR --password-stdin
